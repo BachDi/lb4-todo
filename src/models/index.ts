0439c2ac9e4631093d9b9689f1c68880130efac8
Vu Hoang Phuc <vuhoangphuc272@gmail.com>
2021-11-23 11:17:23 +0700

    wip: getting started with example

---
+++ src/models/index.ts
@@ -0,0 +1,4 @@
+export * from './todo.model';
+export * from './todo-list.model';
+export * from './user.model';
+export * from './project.model';
---
+++ src/repositories/index.ts
@@ -0,0 +1,4 @@
+export * from './todo.repository';
+export * from './todo-list.repository';
+export * from './user.repository';
+export * from './project.repository';
